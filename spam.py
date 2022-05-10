35c696d19455ec4a3ff8891b735238d7a88874e1
Mochamad Wahyu H <90878758+WahyuQwe12@users.noreply.github.com>
2022-05-10 22:54:08 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -2,10 +2,10 @@ import requests,os,sys,time
 from time import sleep
 print ('Contoh nomor 8xxxxxxx')
 
-nomor = input('Masukan Nomor :')
+nomor = input('Masukan Nomor :82245400544')
 print ('- - - JUMLAH SPAM - - -')
 print ('Input 1x Spam = 5SMS')
-jm = int(input('Jumlah Spam :'))
+jm = int(input('Jumlah Spam :1'))
 
 for i in range(jm):
       req=requests.get("https://amfcode.my.id/api/spamsms",params={"phone":nomor}).text
